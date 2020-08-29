fc1f2bd03f3fb2186f5ae12b442ea17c88a84e32
Sunaipaa-Kun <skamickrahaman12@gmail.com>
2020-08-29 13:29:01 +0530

    throwing mechanic done, mostly

---
+++ 2dgame/Assets/Scripts/StickStick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class StickStick : MonoBehaviour
+{
+    [Tooltip("tag of objects to stick to")]
+    [SerializeField]
+    private string[] _stickingTags = { "Sticker" };
+
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.name + $": {_stickingTags.Any(col.gameObject.tag.Contains)}");
+        if(_stickingTags.Any(col.gameObject.tag.Contains)) //for each string in _stickingTags, check if the collided object's tag contain the string
+        {
+            _rb.velocity = Vector3.zero;
+            _rb.angularVelocity = 0f;
+        }
+    }
+}
---
+++ 2dgame/Assets/Scripts/StickThrow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickThrow : MonoBehaviour
+{
+    public float throwPower = 80f;
+    public float pickupDistance = 2;
+
+    [SerializeField] private GameObject _stickPrefab; //SerializeField makes private fields show up in inspector
+    [SerializeField] private float _rotationSpeed = 30f;
+
+    private Camera _mainCam;
+    private Transform _stickThrown;
+
+    void Start()
+    {
+        _mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {    
+            if(_stickThrown)
+            {
+                if(Vector2.Distance(transform.position, _stickThrown.position) < pickupDistance)
+                {
+                    Destroy(_stickThrown.gameObject);
+                    _stickThrown = null;
+                }
+            }
+            else
+            {
+                var dir = ((Vector2)_mainCam.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position).normalized;
+                var stick = Instantiate(_stickPrefab, transform.position,
+                        Quaternion.Euler(Vector3.forward * (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f))) //makes the stick face forward when shot
+                    .GetComponent<Rigidbody2D>();
+                stick.AddForce(dir * throwPower);
+                stick.AddTorque(_rotationSpeed);
+
+                _stickThrown = stick.transform;                
+            }
+        }
+    }
+}
