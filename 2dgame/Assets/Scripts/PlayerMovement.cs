da78a4b32d62325d0b4776f0847d76c21c83ba56
Sunaipaa-Kun <skamickrahaman12@gmail.com>
2020-08-29 11:22:41 +0530

    placed script in correct place, modified it a bit for being more efficient and set gravity to 0 cus its top-down

---
+++ 2dgame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]//makes sure the object this script is being added to has a rigidbody2d component
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementspeed = 5f;
+    private Rigidbody2D rb; //making them private cus they wont be accessed through outside code not inspector
+    private Animator animator; // same ^
+
+
+    Vector2 movement;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(animator)//checks if animator is assigned/found using getcomponent
+        {
+            animator.SetFloat("horizontal", movement.x);
+            animator.SetFloat("vertical", movement.y);
+            animator.SetFloat("speed", movement.sqrMagnitude);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementspeed * Time.fixedDeltaTime);
+    }
+
+
+
+
+
+
+
+}
