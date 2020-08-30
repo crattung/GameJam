8183da16a64565ac1a0ca16320ed03533b9f8d1d
Sunaipaa-Kun <skamickrahaman12@gmail.com>
2020-08-30 16:51:15 +0530

    player animation done

--- 2dgame/Assets/Scripts/StickThrow.cs
+++ 2dgame/Assets/Scripts/StickThrow.cs
@@ -12,14 +12,17 @@ public class StickThrow : MonoBehaviour
 
     private Camera _mainCam;
     private Transform _stickThrown;
+    private Animator _anim;
 
     void Start()
     {
         _mainCam = Camera.main;
+        _anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        var dir = ((Vector2)(_mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position)).normalized;
         if(Input.GetButtonDown("Fire1"))
         {    
             if(_stickThrown)
@@ -28,19 +31,26 @@ public class StickThrow : MonoBehaviour
                 {
                     Destroy(_stickThrown.gameObject);
                     _stickThrown = null;
+                    _anim?.SetBool("HasStick", true);
                 }
             }
             else
             {
-                var dir = ((Vector2)_mainCam.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position).normalized;
                 var stick = Instantiate(_stickPrefab, transform.position,
                         Quaternion.Euler(Vector3.forward * (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f))) //makes the stick face forward when shot
                     .GetComponent<Rigidbody2D>();
                 stick.AddForce(dir * throwPower);
                 stick.AddTorque(_rotationSpeed);
 
-                _stickThrown = stick.transform;                
+                _stickThrown = stick.transform;
+                _anim?.SetBool("HasStick", false);               
             }
+        }
+
+        if (_anim)
+        {
+            _anim.SetFloat("horizontal", dir.x);
+            _anim.SetFloat("vertical", dir.y);
         }
     }
 }
