047d11cc4d2d8410bad09c0205ffd2fd91b9e3de
Sunaipaa-Kun <skamickrahaman12@gmail.com>
2020-08-31 14:42:45 +0530

    multiple spawn

--- 2dgame/Assets/Scripts/Spawner.cs
+++ 2dgame/Assets/Scripts/Spawner.cs
@@ -5,37 +5,32 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject enemy;
-    float randx;
-    Vector2 wheretospawn;
-        public float spawnpersec = 1f;
+    [SerializeField]
+    private GameObject[] enemies;
+
+    [SerializeField]
+    private Transform[] spawnPoints;
+    public float spawnInterval = 1f;
     float nextspawn = 0.0f;
+    [SerializeField] private int enemiesUnlocked = 1;
+    public int EnemiesUnlocked { set { enemiesUnlocked = Mathf.Min(value, enemies.Length); } }
 
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Time.time > nextspawn) 
         {
-            nextspawn = Time.time + spawnpersec;
-                randx = Random.Range(-10f, 10f);
-                wheretospawn = new Vector2(randx, transform.position.y);
-            Instantiate(enemy, wheretospawn, Quaternion.identity);
-
-
-
-
-
-
+            var pointCnt = spawnPoints.Length;
+            var enemyCnt = enemies.Length;
+            if(pointCnt * enemyCnt * enemiesUnlocked > 0)
+            {
+                nextspawn = Time.time + spawnInterval;
+                //randx = Random.Range(-10f, 10f);
+                var wheretospawn = spawnPoints[Random.Range(0, pointCnt)].position;
+                var enemy = enemies[Random.Range(0, enemiesUnlocked)];
+                Instantiate(enemy, wheretospawn, Quaternion.identity);
+            }
         }
     }
 }
