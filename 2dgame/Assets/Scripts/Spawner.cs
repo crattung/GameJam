b5ef2fb5103ced402ac304adba52ea8f1687b03f
crattung <70374697+crattung@users.noreply.github.com>
2020-08-31 12:23:42 +0700

    spawner

---
+++ 2dgame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    float randx;
+    Vector2 wheretospawn;
+        public float spawnpersec = 1f;
+    float nextspawn = 0.0f;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextspawn) 
+        {
+            nextspawn = Time.time + spawnpersec;
+                randx = Random.Range(-10f, 10f);
+                wheretospawn = new Vector2(randx, transform.position.y);
+            Instantiate(enemy, wheretospawn, Quaternion.identity);
+
+
+
+
+
+
+        }
+    }
+}
