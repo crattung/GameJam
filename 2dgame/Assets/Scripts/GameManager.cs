069c5dfebc065d3e89e76ea6e5364168a0ecc68a
Sunaipaa-Kun <skamickrahaman12@gmail.com>
2020-09-01 16:03:27 +0530

    death screen done and show score in the screen

--- 2dgame/Assets/Scripts/GameManager.cs
+++ 2dgame/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -11,11 +12,17 @@ public class GameManager : MonoBehaviour
     public int score = 0;
     [SerializeField] private Health _playerHealth;
     [SerializeField] private Image _healthBar;
+    [SerializeField] private TextMeshProUGUI _currentScoreText;
     [SerializeField] private GameObject _gameOverText;
     [SerializeField] private GameObject _secondaryCam;
 
+    [Header("GameOver")]
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _highScoreText;
+
     void Start()
     {
+        Time.timeScale = 1f;
         if(instance)
             Destroy(this);
         else
@@ -29,13 +36,31 @@ public class GameManager : MonoBehaviour
     public void PlayerDeath()
     {
         Debug.Log("Player death");
-        Destroy(_playerHealth.gameObject);
+        //Destroy(_playerHealth.gameObject);
         _gameOverText.SetActive(true);
-        _secondaryCam.SetActive(true);
+        //set up score
+        var hs = PlayerPrefs.GetInt("highScore", 0);
+        if (score > hs)
+        {
+            hs = score;
+        }
+        PlayerPrefs.SetInt("highScore", hs);
+        _scoreText.text = score.ToString();
+        _highScoreText.text = hs.ToString();
+
+        //_secondaryCam.SetActive(true);
         Time.timeScale = 0f;
     }
     public void PlayerHit()
     {
         Debug.Log("Player hit");
+    }
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
